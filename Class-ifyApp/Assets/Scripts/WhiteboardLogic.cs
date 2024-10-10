88e6cbc76887353ef141023705ccc6f1397693a1
cMitchell3 <mitch452@purdue.edu>
2024-10-09 22:19:08 -0400

    Pulled from develp. Appeasing the great git with a commit

---
+++ Class-ifyApp/Assets/Scripts/WhiteboardLogic.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class WhiteboardLogic : MonoBehaviour
+{
+    public GameObject whiteboardObject;
+    public GameObject playerController;
+
+
+    public void ActivateWhiteboard()
+    {
+        whiteboardObject.SetActive(!whiteboardObject.activeSelf);
+        playerController.GetComponent<PlayerController>().enabled = false;
+    }
+
+    public void Back()
+    {
+        whiteboardObject.SetActive(false);
+        EventSystem.current.SetSelectedGameObject(null);
+        playerController.GetComponent<PlayerController>().enabled = true;
+    }
+}
