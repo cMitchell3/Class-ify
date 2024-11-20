079a41480400debb7ffce094576a92441b41aef6
Clayton Whitten <72474641+ClaytonWhitten@users.noreply.github.com>
2024-11-20 12:28:57 -0500

    Added ability to equip player cosmetics from main menu character customization

--- Class-ifyApp/Assets/Scripts/InventoryLogic.cs
+++ Class-ifyApp/Assets/Scripts/InventoryLogic.cs
@@ -1,13 +1,40 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
+using UnityEngine.UI;
+using System.Collections;
 
 public class InventoryLogic : MonoBehaviour
 {
     [SerializeField] private GameObject playerCosmeticsMenu;
     [SerializeField] private GameObject roomCosmeticsMenu;
+    private CosmeticLogic cosmeticLogic;
 
+    private Button topHatButton;
+    private Button bucketHatButton;
+    private Button cowHatButton;
+
+
+    void Start()
+    {
+        cosmeticLogic = GameObject.FindGameObjectWithTag("CosmeticLogic").GetComponent<CosmeticLogic>();
+
+        StartCoroutine(SetupButtons());
+    }
+
+    private IEnumerator SetupButtons()
+    {
+        // Wait until the buttons exist
+        yield return new WaitUntil(() => GameObject.Find("pixelTopHat") != null);
+
+        topHatButton = GameObject.Find("pixelTopHat").GetComponent<Button>();
+        bucketHatButton = GameObject.Find("pixelBucketHat").GetComponent<Button>();
+        cowHatButton = GameObject.Find("pixelCowboyHat").GetComponent<Button>();
+
+        topHatButton.onClick.AddListener(EquipTopHat);
+        bucketHatButton.onClick.AddListener(EquipBucketHat);
+        cowHatButton.onClick.AddListener(EquipCowHat);
+    }
 
     public void BackButton()
     {
@@ -24,5 +51,36 @@ public class InventoryLogic : MonoBehaviour
     {
         playerCosmeticsMenu.SetActive(false);
         roomCosmeticsMenu.SetActive(true);
+    }
+
+    public void EquipTopHat()
+    {
+        CosmeticLogic.Instance.topHatEquipped = true;
+
+        cosmeticLogic.cowHatEquipped = false;
+        cosmeticLogic.bucketHatEquipped = false;
+    }
+
+    public void EquipCowHat()
+    {
+        cosmeticLogic.cowHatEquipped = true;
+
+        cosmeticLogic.bucketHatEquipped = false;
+        cosmeticLogic.topHatEquipped = false;
+    }
+
+    public void EquipBucketHat()
+    {
+        cosmeticLogic.bucketHatEquipped = true;
+
+        cosmeticLogic.topHatEquipped = false;
+        cosmeticLogic.cowHatEquipped = false;
+    }
+
+    public void EquipNothing()
+    {
+        cosmeticLogic.topHatEquipped = false;
+        cosmeticLogic.cowHatEquipped = false;
+        cosmeticLogic.bucketHatEquipped = false;
     }
 }
